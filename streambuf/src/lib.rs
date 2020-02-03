8e53c7016143229330f338c2159767ce37c8456a
Brian Mason <bpmason1@hotmail.com>
2020-02-03 06:02:54 -0500

    start work on more robust streambuf

--- streambuf/src/lib.rs
+++ streambuf/src/lib.rs
@@ -1,3 +1,5 @@
+extern crate twoway;
+
 use std::io::BufReader;
 use std::io::Error;
 use std::io::prelude::*;
@@ -6,6 +8,54 @@ use std::thread;
 use std::time::{self, Duration};
 
 const CRLF: &str = "\r\n";
+
+pub struct StreamBuf {
+    inner: TcpStream,
+    buffer: Vec<u8>,
+}
+
+pub fn new(inner: TcpStream) -> StreamBuf {
+    StreamBuf{
+        inner: inner,
+        buffer: Vec::new(),
+    }
+}
+
+impl StreamBuf {
+    fn read_inner_to_buffer(&mut self) -> Result<usize, Error> {
+        let mut tmp_vec = Vec::new();
+        let result = self.inner.read( &mut tmp_vec );
+        if result.is_ok() {
+            self.buffer.append(&mut tmp_vec);
+        }
+        result
+    }
+
+    pub fn read_once_until(&mut self, terminator: &[u8]) -> Option<Vec<u8>> {
+        self.read_inner_to_buffer();
+        
+        if self.buffer.len() < terminator.len() {
+            return None;
+        }
+
+        let start_idx_opt = find_subsequence(self.buffer.as_slice(), terminator);
+        let result: Option<Vec<u8>> = match start_idx_opt {
+            Some(start_idx) => {
+                let end_idx = start_idx + terminator.len();
+                let data: Vec<u8> = self.buffer.drain(..end_idx).collect();
+                Some(data)
+            },
+            None => None
+        };
+        result
+    }
+
+}
+
+
+fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    twoway::find_bytes(haystack, needle)
+}
 
 pub fn read_line(buf: &mut BufReader<TcpStream>) -> Result<String, Error> {
     read_until(buf, CRLF)
