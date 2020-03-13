eae67619f5b3cf7c57ef5b8a2ee6596d4ac548b2
Brian Mason <bpmason1@hotmail.com>
2020-03-13 01:01:14 -0400

    use ptr to copy bytes in vector instead of for-loop

--- bottle/src/httpx/response.rs
+++ bottle/src/httpx/response.rs
@@ -20,6 +20,8 @@ use std::io::{
     prelude::*
 };
 use std::net::TcpStream;
+use std::ptr;
+use std::cmp::min;
 
 
 #[derive(PartialEq, Debug)]
@@ -89,19 +91,19 @@ fn _read_http_response(reader: &mut BufReader<TcpStream>) -> Result<Response<Vec
         response = response.header(header_line.key, header_line.value);
     }
 
-    let mut body = Vec::new();
+    let mut bytes_read = 0;
+    let mut body = vec![0; content_length];
     let mut buf2 = vec![0; content_length];
-    let body_size = reader.read(&mut buf2).unwrap();
-    for i in 0..body_size {
-        body.push(buf2[i]);
+    while bytes_read < content_length {
+        let body_size = reader.read(&mut buf2).unwrap();
+        let bytes_to_copy = min(body_size, content_length - bytes_read);
+        unsafe {
+            ptr::copy_nonoverlapping(buf2.as_ptr(), body.as_mut_ptr().offset(bytes_read as isize), bytes_to_copy);
+        }
+        bytes_read += body_size;
+        // std::thread::sleep(std::time::Duration::from_millis(5));
     }
 
-    // if body.len() >= content_length {
-    //     break;
-    // } else {
-    //     // part of the message is missing ... throttle and retry
-    //     thread::sleep(time::Duration::from_millis(5));
-    // }
     response.body(body)
 }
 
@@ -109,4 +111,90 @@ pub fn read_http_response(stream: TcpStream) -> Result<Response<Vec<u8>>, http::
     let mut reader: BufReader<TcpStream> = BufReader::new(stream);
 
     _read_http_response(&mut reader)
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate rand;
+
+    use super::*;
+    use http::{StatusCode};
+    use mockito::{mock, server_address};
+    use std::net::TcpStream;
+    use rand::{Rng, thread_rng};
+    use rand::distributions::Alphanumeric;
+    
+    #[test]
+    fn test_minimal_get_request() {
+        let _mock = mock("GET", "/hello").create();
+        let mut stream = TcpStream::connect(server_address()).unwrap();
+        stream.write_all("GET /hello HTTP/1.1\r\n\r\n".as_bytes()).unwrap();
+        let resp = read_http_response(stream).unwrap();
+        let content_length = resp.headers()[http::header::CONTENT_LENGTH].to_str().unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(resp.version(), Version::HTTP_11);
+        assert_eq!(content_length, "0");
+        assert!(resp.body().is_empty());
+
+        _mock.assert();
+    }
+
+    #[test]
+    fn test_post_request_with_body() {
+        let mut rng = thread_rng();
+        let rand_len = rng.gen_range(10, 20);
+        let rand_body: String = rng
+            .sample_iter(Alphanumeric)
+            .take(rand_len.clone())
+            .collect();
+
+        let _mock = mock("POST", "/foo/bar").with_body(rand_body.clone()).create();
+
+        // Place a request
+        let mut stream = TcpStream::connect(server_address()).unwrap();
+        stream.write_all("POST /foo/bar HTTP/1.1\r\n\r\n".as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+        // Read the response
+        let resp = read_http_response(stream).unwrap();
+        let body: String = String::from_utf8(resp.body().to_vec()).unwrap();
+        let content_length = resp.headers()[http::header::CONTENT_LENGTH].to_str().unwrap();
+
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(resp.version(), Version::HTTP_11);
+        assert_eq!(content_length, rand_len.to_string());
+        assert_eq!(body.clone(), rand_body);
+        assert_eq!(body.len(), rand_len);
+
+        _mock.assert();
+    }
+
+    #[test]
+    fn test_post_response_with_large_body() {
+        let mut rng = thread_rng();
+        let rand_len: usize = rng.gen_range(1e5 as usize, 1e6 as usize);
+        let rand_body: String = rng
+            .sample_iter(Alphanumeric)
+            .take(rand_len.clone())
+            .collect();
+
+        let _mock = mock("POST", "/foo-bar").with_body(rand_body.clone()).create();  // .expect_at_most(1).create();
+
+        // Place a request
+        let mut stream = TcpStream::connect(server_address()).unwrap();
+        stream.write_all("POST /foo-bar HTTP/1.1\r\n\r\n".as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+        // Read the response
+        let resp_result = read_http_response(stream);
+        assert_eq!(resp_result.is_ok(), true);
+        let resp = resp_result.unwrap();
+
+        let body: String = String::from_utf8(resp.body().to_vec()).unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(body.len(), rand_len);
+        assert_eq!(body, rand_body);
+
+        _mock.assert();
+    }
 }
