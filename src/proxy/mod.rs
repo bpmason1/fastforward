3fccf212e2cd4adeb71a71e34f0d2f0a9de37fb3
Brian Mason <bpmason1@hotmail.com>
2020-03-22 17:22:48 -0400

    return boolean status when writing request to client tcp stream

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -44,29 +44,38 @@ fn write_response(resp: Response<Vec<u8>>, mut client: TcpStream) {
     client.write(resp.body());
 } 
 
-fn write_request(req: Request<Vec<u8>>, mut client: TcpStream) {
+fn write_request(req: Request<Vec<u8>>, mut client: TcpStream) -> bool {
     let req_line = format!("{} {} {:?}\r\n", req.method(), req.uri(), req.version());
-    client.write(req_line.as_bytes());
+    if client.write(req_line.as_bytes()).is_err() {
+        return false;
+    }
 
     for (key, value) in req.headers().iter() {
         match key.as_str() {
             "content-length" => {
                 let body_size = req.body().len();
                 let h: String = format!("{}: {}\r\n", key, body_size);
-                client.write(h.as_bytes());    
+                if client.write(h.as_bytes()).is_err() {
+                    return false;
+                }
             },
             "user-agent" => {
                 // ignore this header
             }
             _ => {
                 let h: String = format!("{}: {}\r\n", key, value.to_str().unwrap());
-                client.write(h.as_bytes());
+                if client.write(h.as_bytes()).is_err() {
+                    return false;
+                }
             }
         };
     }
 
-    client.write(b"\r\n");
-    client.write(req.body());
+    if client.write(b"\r\n").is_err() {
+        return false
+    }
+
+    client.write(req.body()).is_ok()
 }
 
 fn handle_client(stream: TcpStream , director: Director ) {
@@ -80,14 +89,16 @@ fn handle_client(stream: TcpStream , director: Director ) {
             let proxy_addr = req.headers().get(http::header::HOST).unwrap();
             let proxy_stream = TcpStream::connect(from_utf8(proxy_addr.as_bytes()).unwrap()).unwrap();
 
-            write_request(req, proxy_stream.try_clone().unwrap());
-
-            match read_http_response(proxy_stream.try_clone().unwrap()) {
-                Ok(resp) => write_response(resp, stream),
-                Err(_) => {
-                    let resp = Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Vec::new()).unwrap();
-                    write_response(resp, stream)
+            if write_request(req, proxy_stream.try_clone().unwrap()) {
+                match read_http_response(proxy_stream.try_clone().unwrap()) {
+                    Ok(resp) => write_response(resp, stream),
+                    Err(_) => {
+                        let resp = Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Vec::new()).unwrap();
+                        write_response(resp, stream)
+                    }
                 }
+            } else {
+                eprintln!("Error sending request to client")
             }
         }
     };
